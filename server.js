6214e63a9d5aaad2a509b5c2cffecaab61b81fc9
anarenp <anarenp@gmail.com>
2018-02-04 11:16:05 -0500

    commit the hello world response get endpoint

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+
+var app = express();
+
+app.listen(3000,function(){
+
+console.log('Server is up and running on port 3000')
+
+});
+
+app.get('/',function(request,response){
+
+	response.send('Hello World - this is the "get endpoint" ')
+});
